738f949e9adfc195ea2e87d9a6e0561bdd38b592
Nitin Rastogi <nitin1tester@gmail.com>
2026-06-20 10:19:37 +0530

    feat: add function to find the first non-repeated character in a string

--- src/TSExcercise/Interviewpractice.ts
+++ src/TSExcercise/Interviewpractice.ts
@@ -86,3 +86,18 @@ for(let ch of splitStrSeven){
     mapSeven.set(ch,(mapSeven.get(ch)??0)+1);
 }
 console.log(mapSeven);
+
+// 8. Find First Non-Repeated Character
+
+const strEight = "aabbcdde";
+let strEightArr:string[] = strEight.split('');
+let strEightMap = new Map<string,number>();
+for(let ch of strEightArr ){
+    strEightMap.set(ch,(strEightMap.get(ch)??0)+1);
+}
+for(let obj of strEightMap){
+    if(obj[1]===1){
+        console.log(obj[0]);
+        break;
+    }
+}
--- src/TSExcercise/tempCodeRunnerFile.ts
+++ src/TSExcercise/tempCodeRunnerFile.ts
@@ -1,8 +1,13 @@
 
-const strSeven = "playwright";
-let splitStrSeven:string[] = strSeven.split('');
-let mapSeven = new Map<string,number>();
-for(let ch of splitStrSeven){
-    mapSeven.set(ch,(mapSeven.get(ch)??0)+1);
+const strEight = "aabbcdde";
+let strEightArr:string[] = strEight.split('');
+let strEightMap = new Map<string,number>();
+for(let ch of strEightArr ){
+    strEightMap.set(ch,(strEightMap.get(ch)??0)+1);
 }
-console.log(mapSeven);
+for(let obj of strEightMap){
+    if(obj[1]===1){
+        console.log(obj[0]);
+        break;
+    }
+}
